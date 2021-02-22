f01faaf6db007ef360a00088efa4df2f7baeec1b
Seb Bailey <sebastian@mastodonc.com>
2021-02-22 18:04:07 +0000

    Fix object naming

--- src/report_1.R
+++ src/report_1.R
@@ -13,16 +13,18 @@ report_1 <- function(actual_episodes_file) {
   set.seed(5)
   actual_episodes <- read.csv(actual_episodes_file, header = TRUE, 
                               stringsAsFactors = FALSE, na.strings ="NA")
-  episodes$report_date <- ymd(episodes$report_date)
-  episodes$ceased <- ymd(episodes$ceased)
-  birthdays <- episodes %>% 
+  actual_episodes$report_date <- ymd(actual_episodes$report_date)
+  actual_episodes$ceased <- ymd(actual_episodes$ceased)
+  end_date <- max(c(actual_episodes$ceased, actual_episodes$report_date), na.rm = TRUE)
+  birthdays <- actual_episodes %>% 
     group_by(ID) %>% 
-    summarise(birthday = imputed_birthday(DOB[1], min(report_date), coalesce(max(ceased), end_date)))
-  episodes <- episodes %>% inner_join(birthdays)
-  episodes <- episodes %>% 
+    summarise(birthday = impute_birthday(DOB[1], min(report_date), coalesce(max(ceased), end_date)))
+  actual_episodes <- actual_episodes %>% inner_join(birthdays)
+  actual_episodes <- actual_episodes %>% 
     group_by(phase_id) %>% 
     mutate(admission_age = year_diff(min(birthday), min(report_date))) %>% 
     ungroup
 }
 
+# actual_episodes_file <- '~/code/witan.cic/data/episodes.scrubbed.csv'
 # report_1('~/code/witan.cic/data/episodes.scrubbed.csv')
