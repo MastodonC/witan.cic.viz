2e41a9296f6783d6ba20e6f1b494bea9df716da1
Seb Bailey <sebastian@mastodonc.com>
2021-02-22 17:55:05 +0000

    Impute birthdays

--- src/report_1.R
+++ src/report_1.R
@@ -2,6 +2,12 @@
 loadfonts()
 ### `font_import()` should be run initially to ensure required fonts are present
 
+impute_birthday <- function(birth_month, min_start, max_cease) {
+  earliest_possible <- max(max_cease - days(floor(18 * 365.25)) + 1, month_start(birth_month))
+  latest_possible <- min(min_start, month_end(birth_month))
+  date_between(earliest_possible, latest_possible)
+}
+
 report_1 <- function(actual_episodes_file) {
   set.seed(5)
   actual_episodes <- read.csv(actual_episodes_file, header = TRUE, 
