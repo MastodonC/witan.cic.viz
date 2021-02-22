6ed05e7791ede0e444b870cc0798c08259177b57
Seb Bailey <sebastian@mastodonc.com>
2021-02-22 17:52:15 +0000

    Convert to YMD format

--- src/report_1.R
+++ src/report_1.R
@@ -4,5 +4,8 @@ loadfonts()
 
 report_1 <- function(actual_episodes_file) {
   set.seed(5)
-  actual_episodes <- read.csv(actual_episodes_file, header = TRUE, stringsAsFactors = FALSE, na.strings ="NA")
+  actual_episodes <- read.csv(actual_episodes_file, header = TRUE, 
+                              stringsAsFactors = FALSE, na.strings ="NA")
+  episodes$report_date <- ymd(episodes$report_date)
+  episodes$ceased <- ymd(episodes$ceased)
 }
