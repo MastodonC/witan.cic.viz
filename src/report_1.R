f4c52a77979cd21e62d04928e65b162bc6a21364
Seb Bailey <sebastian@mastodonc.com>
2021-02-22 17:41:55 +0000

    Read in actual episodes file

--- src/report_1.R
+++ src/report_1.R
@@ -1,3 +1,8 @@
 
 loadfonts()
 ### `font_import()` should be run initially to ensure required fonts are present
+
+report_1 <- function(actual_episodes_file) {
+  set.seed(5)
+  actual_episodes <- read.csv(actual_episodes_file, header = TRUE, stringsAsFactors = FALSE, na.strings ="NA")
+}
