cc2700c30af38d20c70930cd565e7b852422817a
Seb Bailey <sebastian@mastodonc.com>
2021-03-17 13:28:59 +0000

    Standardise colours

--- src/report_1.R
+++ src/report_1.R
@@ -63,8 +63,8 @@ report_1 <- function(actual_episodes_file, projected_episodes_file = NULL, count
   text(5, 6, "\"LA\" CiC Projection", cex=2.5) ## TODO take LA name from config
   
   set.seed(5)
-  colours <- c("#4E79A7", "#F28E2B", "grey", "#F28E2B", "#4E79A7", "black", "blue")
-  names(colours) <- c("lower.ci", "q1", "median", "q3", "upper.ci", "SSDA903", "MIS")
+  colours <- c("gray", "#778899", "gray", "black", "blue")
+  names(colours) <- c("Confidence Interval", "Interquartile Range", "Projected", "SSDA903", "MIS")
   dates <- seq(as.Date("2016-01-01"), projection_end, by = "week") ## TODO take dates from config file
   tableau_color_pal("Tableau 20")(20)
   
