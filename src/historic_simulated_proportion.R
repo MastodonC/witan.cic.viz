45f225224bfa021c3476cdcffb20e7399254a421
Henry Garner <henry@henrygarner.com>
2021-06-10 20:38:39 +0100

    Simulated proportion CI shouldn't include historic proportion CI

--- src/historic_simulated_proportion.R
+++ src/historic_simulated_proportion.R
@@ -10,12 +10,12 @@ generate_historic_simulated_area_plot <- function(input_dir, output_dir, histori
     dplyr::select(ID, simulation, period_start, period_end, birthday, provenance)
   
   n_cic <- data.frame(date = seq(historic_start, projection_end, by = "month")) %>%
-    inner_join(projected_periods, by = character()) %>%
-    filter(period_start <= date & period_end >= date) %>%
-    mutate(provenance = if_else(provenance == "S", "Simulated joiners", "Historic joiners")) %>%
-    group_by(simulation, date, provenance) %>%
-    summarise(n_cic = n()) %>%
-    mutate(provenance = factor(provenance, levels = c("Simulated joiners", "Historic joiners")))
+    dplyr::inner_join(projected_periods, by = character()) %>%
+    dplyr::filter(period_start <= date & period_end >= date) %>%
+    dplyr::mutate(provenance = if_else(provenance == "S", "Simulated joiners", "Historic joiners")) %>%
+    dplyr::group_by(simulation, date, provenance) %>%
+    dplyr::summarise(n_cic = n()) %>%
+    dplyr::mutate(provenance = factor(provenance, levels = c("Simulated joiners", "Historic joiners")))
   
   n_cic <- n_cic %>%
     inner_join(grid_x_labels)
@@ -38,27 +38,27 @@ generate_historic_simulated_area_plot <- function(input_dir, output_dir, histori
                             upper_95 = quantile(n_cic, 0.75)),
                 aes(x = date, ymin = lower_95, ymax = upper_95), fill = "black", alpha = 0.2) +
     geom_ribbon(data = n_cic %>%
-                  group_by(label, date, simulation) %>%
+                  group_by(date, provenance, simulation) %>%
                   summarise(n_cic = sum(n_cic)) %>%
-                  summarise(lower_95 = quantile(n_cic, 0.0275),
-                            upper_95 = quantile(n_cic, 0.975)),
+                  group_by(date) %>%
+                  mutate(median_historic = median(if_else(provenance == "Historic joiners", n_cic, NULL), na.rm = TRUE)) %>%
+                  filter(provenance != "Historic joiners") %>%
+                  summarise(lower_95 = quantile(n_cic, 0.0275) + median_historic,
+                            upper_95 = quantile(n_cic, 0.975) + median_historic),
                 aes(x = date, ymin = lower_95, ymax = upper_95), fill = "black", alpha = 0.2) +
     geom_ribbon(data = n_cic %>%
-                  group_by(label, date, simulation) %>%
+                  group_by(date, provenance, simulation) %>%
                   summarise(n_cic = sum(n_cic)) %>%
-                  summarise(lower_95 = quantile(n_cic, 0.25),
-                            upper_95 = quantile(n_cic, 0.75)),
+                  group_by(date) %>%
+                  mutate(median_historic = median(if_else(provenance == "Historic joiners", n_cic, NULL), na.rm = TRUE)) %>%
+                  filter(provenance != "Historic joiners") %>%
+                  summarise(lower_95 = quantile(n_cic, 0.25) + median_historic,
+                            upper_95 = quantile(n_cic, 0.75) + median_historic),
                 aes(x = date, ymin = lower_95, ymax = upper_95), fill = "black", alpha = 0.2) +
-    geom_line(data = n_cic %>%
-                group_by(label, date, simulation) %>%
-                filter(date >= projection_start) %>%
-                summarise(n_cic = sum(n_cic)) %>%
-                summarise(y = median(n_cic)),
-              aes(x = date, y = y), colour = "black", linetype = 2, alpha = 0.5) +
     scale_fill_manual(values = tableau_color_pal("Tableau 20")(4)[c(3,1)]) +
-    labs(y = "CiC", x = "Date", fill = "Joiners") +
-    ylim(c(0, max_y)) +
-    facet_grid(cols = vars(label))
+    labs(y = "CiC count", x = "Date", fill = "Joiners") +
+    coord_cartesian(ylim = c(0, max_y))
+    # facet_grid(cols = vars(label))
   ggsave(file.path(output_dir, "historic-simulated-proportion.png"), width = 8, height = 5)
   
   n_cic %>%
@@ -71,13 +71,14 @@ generate_historic_simulated_area_plot <- function(input_dir, output_dir, histori
     write.csv(file = file.path(output_dir, "historic-simulated-proportion.csv"), row.names = FALSE)
 }
 
-input_dir <- '/Users/henry/Mastodon C/witan.cic/data/scc/2021-06-02/scenario/outputs'
-output_dir <- '/Users/henry/Mastodon C/witan.cic/data/scc/2021-06-02/scenario/outputs'
-historic_start <- as.Date("2014-03-01")
-historic_end <-  as.Date("2020-03-31")
-projection_start <- as.Date("2019-03-31")
-projection_end <- as.Date("2024-03-01")
-max_y <- 1300
-
-# generate_historic_simulated_area_plot("/Users/henry/Mastodon C/witan.cic/data/scc/2021-04-08/trended", "/Users/henry/Mastodon C/witan.cic/data/scc/2021-04-08/trended",
-#                                       as.Date("2015-01-01"), as.Date("2020-03-31"), as.Date("2019-03-31"), as.Date("2024-03-31"), 1300)
+# input_dir <- ''
+# output_dir <- ''
+# historic_start <- as.Date("2014-03-01")
+# historic_end <-  as.Date("2020-03-31")
+# projection_start <- as.Date("2019-03-31")
+# projection_end <- as.Date("2024-03-01")
+# max_y <- 1300
+# 
+# generate_historic_simulated_area_plot(input_dir, output_dir,
+#                                       historic_start, historic_end, projection_start, projection_end, 1300)
+# 
