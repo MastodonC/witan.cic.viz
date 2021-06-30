e2e90aebc7c233e6d89f2efc4544c967e8cb2ad1
Henry Garner <henry@henrygarner.com>
2021-06-30 11:12:12 +0100

    Fix date sequencing and create 100% stacked variations

--- src/cic_placement_age_group.R
+++ src/cic_placement_age_group.R
@@ -21,8 +21,9 @@ cic_placement_age_group <- function(input_dir, output_dir, from_date, to_date, g
     arrange(desc(n)) %>%
     pull(placement)
   
+
   chart_data <- episodes %>%
-    dplyr::inner_join(data.frame(month = floor_date(seq(from_date, to_date, by = "month"), unit = "month")), by = character()) %>%
+    dplyr::inner_join(data.frame(month = floor_date(seq(month_start(from_date), month_start(to_date), by = "month"), unit = "month")), by = character()) %>%
     dplyr::filter(start <= month & end >= month) %>%
     dplyr::mutate(age_group = age_category(year_diff(birthday, month), group_ages)) %>%
     dplyr::group_by(month, simulation, period_id) %>%
@@ -30,7 +31,7 @@ cic_placement_age_group <- function(input_dir, output_dir, from_date, to_date, g
     dplyr::group_by(month, age_group, placement, simulation) %>%
     dplyr::summarise(n = n_distinct(period_id), .groups = "drop_last") %>%
     dplyr::summarise(n = median(n))
-  
+
   categories <- if(group_ages) {age_categories} else {age_labels}
   category_labels <- paste(categories, "count of children")
   names(category_labels) <- categories
@@ -42,17 +43,46 @@ cic_placement_age_group <- function(input_dir, output_dir, from_date, to_date, g
   print(ggplot(chart_data %>% group_by(month, age_group) %>% summarise(n = sum(n)), aes(month, n, fill = age_group)) +
     geom_bar(stat = "identity") +
     labs(x = "Month", y = "Children in care", fill = "Age group", title = "Children in care by age") +
-    scale_fill_manual(values = tableau_color_pal("Tableau 20")(20))
-    # xlim(c(from_date, as.Date("2024-03-31"))) + ylim(c(0, 500)) # Required for 'what happens next?'
+    scale_fill_manual(values = tableau_color_pal("Tableau 20")(20)) +
+    xlim(c(from_date, as.Date("2024-03-31"))) + ylim(c(0, 500)) # Required for 'what happens next?'
     )
+
+  print(ggplot(chart_data %>% group_by(month, age_group) %>% summarise(n = sum(n)), aes(month, n, fill = age_group)) +
+          geom_bar(stat = "identity", position = "fill") +
+          labs(x = "Month", y = "Children in care", fill = "Age group", title = "Children in care by age") +
+          scale_fill_manual(values = tableau_color_pal("Tableau 20")(20)) +
+          scale_y_continuous(breaks = seq(0, 1, by = 0.2), labels = paste0(seq(0, 100, by = 20), "%")) +
+          xlim(c(from_date, as.Date("2024-03-31"))) # Required for 'what happens next?'
+  )
+
+  print(ggplot(chart_data %>% group_by(month, age_group) %>% summarise(n = sum(n)), aes(month, n, fill = age_group)) +
+          geom_bar(stat = "identity") +
+          labs(x = "Month", y = "Children in care", fill = "Age group", title = "Children in care by age") +
+          scale_fill_manual(values = tableau_color_pal("Tableau 20")(20))
+  )
+
+  print(ggplot(chart_data %>% group_by(month, age_group) %>% summarise(n = sum(n)), aes(month, n, fill = age_group)) +
+          geom_bar(stat = "identity", position = "fill") +
+          labs(x = "Month", y = "Children in care", fill = "Age group", title = "Children in care by age") +
+          scale_fill_manual(values = tableau_color_pal("Tableau 20")(20)) +
+          scale_y_continuous(breaks = seq(0, 1, by = 0.2), labels = paste0(seq(0, 100, by = 20), "%"))
+  )
 
   for (the.placement in placements) {
     print(ggplot(chart_data %>% filter(placement == the.placement), aes(month, n, fill = age_group)) +
       geom_bar(stat = "identity") +
       labs(x = "Month", y = "Children in care", fill = "Age group", title = paste("Children in", the.placement)) +
-      scale_fill_manual(values = tableau_color_pal("Tableau 20")(20)))
+      scale_fill_manual(values = tableau_color_pal("Tableau 20")(20))
+      )
+
+    print(ggplot(chart_data %>% filter(placement == the.placement), aes(month, n, fill = age_group)) +
+            geom_bar(stat = "identity", position = "fill") +
+            labs(x = "Month", y = "Children in care", fill = "Age group", title = paste("Children in", the.placement)) +
+            scale_fill_manual(values = tableau_color_pal("Tableau 20")(20)) +
+            scale_y_continuous(breaks = seq(0, 1, by = 0.2), labels = paste0(seq(0, 100, by = 20), "%"))
+    )
   }
-  
+
   all.placements <- c("A3", "A4", "A5", "A6", "H5", "K1", "K2", "M2", "M3", "P1", "P2",
                       "Q1","Q2", "R1", "R2", "R3", "R5", "S1", "T0", "T4", "Z1",
                       'Join')
@@ -65,11 +95,26 @@ cic_placement_age_group <- function(input_dir, output_dir, from_date, to_date, g
           labs(x = "Month", y = "Children in care", fill = "Placement", title = "Children in care by placement") +
           scale_fill_manual(values = placement.colours))
   
+  print(ggplot(chart_data %>% group_by(month, placement) %>% summarise(n = sum(n)), aes(month, n, fill = placement)) +
+          geom_bar(stat = "identity", position = "fill") +
+          labs(x = "Month", y = "Children in care", fill = "Placement", title = "Children in care by placement") +
+          scale_fill_manual(values = placement.colours) +
+          scale_y_continuous(breaks = seq(0, 1, by = 0.2), labels = paste0(seq(0, 100, by = 20), "%"))
+        )
+  
   for (age.group in categories) {
     print(ggplot(chart_data %>% filter(age_group == age.group), aes(month, n, fill = placement)) +
             geom_bar(stat = "identity") +
             labs(x = "Month", y = "Children in care", fill = "Placement", title = paste(age.group, "children")) +
-            scale_fill_manual(values = placement.colours))
+            scale_fill_manual(values = placement.colours)
+    )
+    
+    print(ggplot(chart_data %>% filter(age_group == age.group), aes(month, n, fill = placement)) +
+            geom_bar(stat = "identity", position = "fill") +
+            labs(x = "Month", y = "Children in care", fill = "Placement", title = paste(age.group, "children")) +
+            scale_fill_manual(values = placement.colours) +
+            scale_y_continuous(breaks = seq(0, 1, by = 0.2), labels = paste0(seq(0, 100, by = 20), "%"))
+    )
   }
   
   dev.off()
@@ -81,4 +126,4 @@ cic_placement_age_group <- function(input_dir, output_dir, from_date, to_date, g
 # to_date <- as.Date("2021-03-31")
 # group_ages <- TRUE
 # 
-# cic_placement_age_group(input_dir, output_dir, from_date, to_date, group_ages)
+cic_placement_age_group(input_dir, output_dir, from_date, to_date, group_ages)
