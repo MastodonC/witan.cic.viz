572863de018b8911a739e3205c5d0d5bddd4343a
Seb Bailey <seb231@me.com>
2022-08-22 12:19:22 +0100

    Output placements by age

--- src/cic_placement_age_group.R
+++ src/cic_placement_age_group.R
@@ -8,6 +8,7 @@ cic_placement_age_group <- function(input_dir, output_dir, from_date, to_date, g
   
   projection_episodes_file <- "projection-episodes.csv"
   cic_by_age_group_pdf <- "cic-placement-age-group.pdf"
+  cic_by_age_pdf <- "cic-placement-age.pdf"
   
   episodes <- read.csv(file.path(input_dir, projection_episodes_file)) %>%
     dplyr::mutate(period_id = ID, period_start = ymd(Period.Start), period_end = ymd(Period.End),
@@ -118,6 +119,52 @@ cic_placement_age_group <- function(input_dir, output_dir, from_date, to_date, g
   }
   
   dev.off()
+  
+  chart_data <- episodes %>%
+    dplyr::inner_join(data.frame(month = floor_date(seq(month_start(from_date), month_start(to_date), by = "month"), unit = "month")), by = character()) %>%
+    dplyr::filter(start <= month & end >= month) %>%
+    dplyr::mutate(age = year_diff(birthday, month)) %>%
+    dplyr::group_by(month, simulation, period_id) %>%
+    dplyr::slice(1) %>%
+    dplyr::group_by(month, age, placement, simulation) %>%
+    dplyr::summarise(n = n_distinct(period_id), .groups = "drop_last") %>%
+    dplyr::summarise(n = median(n))
+  
+  write.csv(chart_data, file = file.path(output_dir, "cic-placement-age.csv"), row.names = FALSE)
+  
+  pdf(file = file.path(output_dir, cic_by_age_pdf))
+  
+  for (the.placement in placements) {
+    print(ggplot(chart_data %>% filter(placement == the.placement), aes(month, n, fill = factor(age))) +
+            geom_bar(stat = "identity") +
+            labs(x = "Month", y = "Children in care", fill = "Age", title = paste("Children in", the.placement)) +
+            scale_fill_manual(values = tableau_color_pal("Tableau 20")(20))
+    )
+    print(ggplot(chart_data %>% filter(placement == the.placement), aes(month, n, fill = factor(age))) +
+            geom_bar(stat = "identity", position = "fill") +
+            labs(x = "Month", y = "Children in care", fill = "Age", title = paste("Children in", the.placement)) +
+            scale_fill_manual(values = tableau_color_pal("Tableau 20")(20)) +
+            scale_y_continuous(breaks = seq(0, 1, by = 0.2), labels = paste0(seq(0, 100, by = 20), "%"))
+    )
+  }
+  
+  for (age in age_labels) {
+    print(ggplot(chart_data %>% filter(age == age), aes(month, n, fill = placement)) +
+            geom_bar(stat = "identity") +
+            labs(x = "Month", y = "Children in care", fill = "Placement", title = paste(age, "children")) +
+            scale_fill_manual(values = placement.colours)
+    )
+    
+    print(ggplot(chart_data %>% filter(age == age), aes(month, n, fill = placement)) +
+            geom_bar(stat = "identity", position = "fill") +
+            labs(x = "Month", y = "Children in care", fill = "Placement", title = paste(age, "children")) +
+            scale_fill_manual(values = placement.colours) +
+            scale_y_continuous(breaks = seq(0, 1, by = 0.2), labels = paste0(seq(0, 100, by = 20), "%"))
+    )
+  }
+  
+  dev.off()
+
 }
 
 # input_dir <- ''
